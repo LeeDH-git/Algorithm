a8273ba403360d2a4000511ffa68fdd1aebbdd2b
LeeDH-git <djrl02@naver.com>
2021-01-08 15:56:26 +0900

    dd

--- src/Algorithm/Sort/BubbleSort.java
+++ src/Algorithm/Sort/BubbleSort.java
@@ -6,28 +6,24 @@ public class BubbleSort {
      * 13 23 11 7 5 15
      * 출력 예제
      * 5 7 11 13 15 23 */
-
-    public static void bubblesort(int[] arr) {
+    public static void main(String[] args) {
         /* 배열을 순차탐색하여 i, i+1번째 요소를 비교하여 큰 것을 뒤로 이동
-         * 위 과정이 1번 처리되는 경우 가장 큰 수가 배열 마지막에 위치 함
-         * 다음 탐색 부터는 마지막 요소는 안해도 됨 (그래서 내부에 있는 for문은 arr.length - i 까지만 탐색) */
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j + 1];
-                    arr[j + 1] = arr[j];
-                    arr[j] = temp;
+         * 위 과정이 처음 처리될때 가장 큰 수가 배열 마지막에 오도록 정렬이 됨
+         * 따라서 그 다음 탐색 부터는 마지막은 비교를 안해도 됨
+         * 그러므로 내부에 있는 for문은 a.length - i 까지만 탐색한다. */
+        int[] a = {13, 23, 11, 7, 5, 15};
+        int b;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    b = a[j + 1];
+                    a[j + 1] = a[j];
+                    a[j] = b;
                 }
             }
         }
 
-    }
-
-    public static void main(String[] args) {
-        int[] arr = new int[]{13, 23, 11, 7, 5, 15};
-        BubbleSort.bubblesort(arr);
-        for (int i : arr) {
+        for (int i : a) {
             System.out.print(i + " ");
         }
     }
--- src/Algorithm/Sort/BubbleSort2.java
+++ src/Algorithm/Sort/BubbleSort2.java
@@ -1,5 +1,9 @@
 package Algorithm.Sort;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BubbleSort2 {
     /* N개의 정수가 입력되면 음의 정수는 앞쪽에 양의 정수는 뒷쪽에 있어야 한다.
      * 또한 양의 정수와 음의 정수의 순서에는 변함이 없어야 함
@@ -8,7 +12,18 @@ public class BubbleSort2 {
      * 출력 예제
      * -3 -2 -6 1 2 3 5 6*/
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.print("배열의 크기를 입력하세요 : ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
 
+        int[] a = new int[num];
+
+        System.out.println("배열크기 : " + a.length);
+
+        for (int i = 0; i < num; i++) {
+            System.out.println("정수를 입력하세요 : ");
+            a[i] = Integer.parseInt(br.readLine());
+        }
     }
 }
