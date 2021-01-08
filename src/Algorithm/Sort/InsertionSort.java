9c5dcf096708eb162549b28ceb1ad4c6b57dd780
LeeDH-git <djrl02@naver.com>
2021-01-08 17:42:45 +0900

    dd

--- src/Algorithm/Sort/InsertionSort.java
+++ src/Algorithm/Sort/InsertionSort.java
@@ -7,17 +7,22 @@ public class InsertionSort {
      * 출력예제
      * 5 6 7 9 10 11*/
     public static void main(String[] args) {
+        /* 삽입정렬은 앞에서부터 차례대로 배열의 두번째 부터 비교를 하며 연산을 한다
+         * 첫번째 11과 7을 비교하여 기준값인 7이 11보다 작으므로 7의 자리에 11을 삽입
+         * 이제 다음 인덱스인 5가 기준값이 되어 7과 비교 기준값인 5보다 7이 크므로 기준값인 5의 자리에 7을 삽입한다
+         * 이런식으로 인덱스가 끝날때까지 비교를 반복한다
+         */
         int[] a = {11, 7, 5, 6, 10, 9};
 
         for (int i = 1; i < a.length; i++) {
-            int standard = a[i];  // 기준
-            int aux = i - 1;   // 비교할 대상
+            int std = a[i];  // 기준
+            int compv  = i - 1;   // 비교할 대상
 
-            while (aux >= 0 && standard < a[aux]) {
-                a[aux + 1] = a[aux];   // 비교대상이 큰 경우 오른쪽으로 밀어냄
-                aux--;
+            while (compv >= 0 && std < a[compv]) {
+                a[compv + 1] = a[compv];   // 비교대상이 큰 경우 오른쪽으로 밀어냄
+                compv--;
             }
-            a[aux + 1] = standard;  // 기준값 저장
+            a[compv + 1] = std;  // 기준값 저장
         }
 
         for (int i : a) {
